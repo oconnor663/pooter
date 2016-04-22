714d4c984d9a8ea1c388612bfacc6975c244a3b7
Jack O'Connor <oconnor663@gmail.com>
2016-04-21 23:30:39 -0400

    add a pooter button and a counter

--- index.android.js
+++ index.android.js
@@ -11,21 +11,37 @@ import React, {
   View
 } from 'react-native';
 
+import Button from 'react-native-button';
+
 class pooter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: 0,
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
+        <Button
+          style={styles.pooter}
+          onPress={this.pooterClick.bind(this)}
+        >
+          Pooter!
+        </Button>
+        <Text style={styles.counter}>
+          {this.state.num}
         </Text>
       </View>
     );
+  }
+
+  pooterClick(event) {
+    console.log('Pressed!');
+    this.setState({
+      num: this.state.num + 1,
+    })
   }
 }
 
@@ -36,15 +52,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  pooter: {
+    fontSize: 80,
+    backgroundColor: 'blue',
+    color: 'white',
+    padding: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  counter: {
+    fontSize: 80,
   },
 });
 
